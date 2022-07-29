351c107c298fce9798fb3d483df851b551c95d24
Hardikkaneriya <45586073+Hardikkaneriya@users.noreply.github.com>
2022-07-29 20:32:21 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup ,find_packages
+from setuptools import setup ,find_packages 
 
 with open("requirements.txt") as requirement_file:
     requirements = requirement_file.read().split()
