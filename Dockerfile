43c684dda7da7b3c63ff23f5cafd8ccf2ead8b0a
Hardikkaneriya <45586073+Hardikkaneriya@users.noreply.github.com>
2022-07-29 20:29:16 +0530

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+
+FROM python:3.7-slim
+
+COPY requirements.txt /
+
+RUN pip install --no-cache-dir --trusted-host pypi.python.org -r requirements.txt
+
+WORKDIR /app
+ADD . /app
+
+RUN pip install .
+
+# ENTRYPOINT ["python", "./srs/main.py" ,  ,"Berlin" , "--duration" , "10"]
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup ,find_packages
+
+with open("requirements.txt") as requirement_file:
+    requirements = requirement_file.read().split()
+
+setup(
+    name='Tour_Guide',
+    version='1.0.0',
+    description='this package is for tour guide program',
+    install_requires=requirements,
+    packages=find_packages(exclude='test*'),
+    setup_requires=['wheel'],
+    entry_points={
+        'console_scripts': [
+            'abc = srs.main:main'
+        ],
+    },
+)
